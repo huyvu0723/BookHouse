6c8e9390bf8a44580f5153681581b242f0e03087
huyvu0723 <33371542+huyvu0723@users.noreply.github.com>
2018-10-23 01:51:09 +0700

    Add read pdf

--- app/src/main/java/vu/huy/bookhouse/Activity/DashboardActivity.java
+++ app/src/main/java/vu/huy/bookhouse/Activity/DashboardActivity.java
@@ -13,6 +13,9 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.FrameLayout;
 
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.util.FitPolicy;
+
 import vu.huy.bookhouse.R;
 import vu.huy.bookhouse.Fragment.AccountFragment;
 import vu.huy.bookhouse.Fragment.HomeFragment;
@@ -48,6 +51,28 @@ public class DashboardActivity extends AppCompatActivity {
             }
         });
 
+        //get pdf file
+        PDFView pdfView = findViewById(R.id.pdfView);
+        pdfView.fromAsset("pdfexample.pdf")
+                .pages(0, 2, 1, 3, 3, 3) // all pages are displayed by default
+                .enableSwipe(true) // allows to block changing pages using swipe
+                .swipeHorizontal(false)
+                .enableDoubletap(true)
+                .defaultPage(0)
+                // allows to draw something on the current page, usually visible in the middle of the screen
+
+                .enableAnnotationRendering(false) // render annotations (such as comments, colors or forms)
+                .password(null)
+                .scrollHandle(null)
+                .enableAntialiasing(true) // improve rendering a little bit on low-res screens
+                // spacing between pages in dp. To define spacing color, set view background
+                .spacing(0)
+                .autoSpacing(false) // add dynamic spacing to fit each page on its own on the screen
+                .pageFitPolicy(FitPolicy.WIDTH)
+                .pageSnap(true) // snap pages to screen boundaries
+                .pageFling(false) // make a fling change only a single page like ViewPager
+                .nightMode(false) // toggle night mode
+                .load();
     }
 
     @Override
---
+++ app/src/main/java/vu/huy/bookhouse/Activity/ReadPDFActivity.java
@@ -0,0 +1,40 @@
+package vu.huy.bookhouse.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.listener.OnPageChangeListener;
+import com.github.barteksc.pdfviewer.util.FitPolicy;
+
+import vu.huy.bookhouse.R;
+
+public class ReadPDFActivity extends AppCompatActivity implements OnPageChangeListener {
+
+    TextView pagePDF;
+    int totalPage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_pdf);
+        pagePDF = findViewById(R.id.page_pdf);
+        pagePDF.bringToFront();
+        PDFView pdfView = findViewById(R.id.pdfView);
+        pdfView.fromAsset("hon.pdf")
+                .enableAntialiasing(true)
+                .enableSwipe(true)
+                .swipeHorizontal(true)
+                .pageFling(true)
+                .onPageChange(this)
+                .load();
+        totalPage = pdfView.getCurrentPage();
+    }
+
+    @Override
+    public void onPageChanged(int page, int pageCount) {
+         pagePDF.setText("" + (page + 1) + " / "+ totalPage);
+        //do what you want with the pageNumber
+    }
+
+}
